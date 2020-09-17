1e0821f7e99b403fc84d2cc6fb5f8fe343a62542
github@cybertekschool.com <github@cybertekschool.com>
2020-09-16 21:31:02 -0500

    added note done completely

--- src/AddClassToGit/HelloGit.java
+++ src/AddClassToGit/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Yhlas");
 		System.out.println("I am done with Git");
+		System.out.println("done");
 	}
 
 }
